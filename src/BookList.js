177ed5710c0e827994e3f9e90822f1e6c9c43a93
Batuhan Ertas <ertas.batuhan@hotmail.com>
2018-07-29 15:30:42 +0200

    feat: gets books from server, creates new component for book list

---
+++ src/BookList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class BookList extends Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelf: PropTypes.string.isRequired,
+  };
+
+  render() {
+    const { books, shelf } = this.props;
+    let booksOfTheCurrentShelf = [];
+
+    if (books.length > 0) {
+      booksOfTheCurrentShelf = books.filter( (book) => (
+        book.shelf === shelf
+      ));
+    }
+
+    return (
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {booksOfTheCurrentShelf.map( book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="move" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+                </div>
+              </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default BookList
